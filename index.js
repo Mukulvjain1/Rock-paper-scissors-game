0e59d42f1e8592a2e214b95e2e2733817f73f58c
mukulvjain1 <mukulvjain1@gmail.com>
2024-02-02 20:28:29 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,90 @@
+let rules = document.querySelector("#rules");
+let gameSec = document.querySelector("#game-section");
+let gameZone = document.querySelector("#game-zone");
+let finalres = document.querySelector("#result");
+let paper = document.querySelector("#selected-paper");
+let scissor = document.querySelector("#selected-scissor");
+let stone = document.querySelector("#selected-stone");
+let score=document.querySelector("#score-val");
+let computerChoice;
+let userChoice;
+let previousRandomNumber;
+
+function resetComputerChoice() {
+  let num = generateUniqueRandomNumber();
+
+  if (num == 1) {
+    computerChoice = "paper";
+    console.log(`computer selected paper`);
+  } else if (num == 2) {
+    computerChoice = "scissor";
+    console.log(`computer selected scissor`);
+  } else {
+    computerChoice = "stone";
+    console.log(`computer selected stone`);
+  }
+}
+
+resetComputerChoice(); // Initialize computerChoice on page load
+
+function handleGameResult() {
+  if (userChoice == computerChoice) {
+    finalres.innerHTML = "WON";
+  } else {
+    finalres.innerHTML = "LOST";
+  }
+}
+function handleScore(){
+    let finalRes=0;
+    score.innerHTML=finalRes
+    if (userChoice == computerChoice) {
+        return finalRes=finalRes+1;
+      } else {
+        return finalRes
+      }
+}
+function handleClick(choice) {
+  userChoice = choice;
+  console.log("User chose:", choice);
+  if (userChoice == "paper") {
+    paper.style.display = "block";
+  } else if (userChoice == "scissor") {
+    scissor.style.display = "block";
+  } else {
+    stone.style.display = "block";
+  }
+  handleGameResult();
+  resetComputerChoice(); // Reset the computerChoice after user makes a choice
+}
+
+function openrules() {
+  rules.style.display = "block";
+}
+
+function closerules() {
+  rules.style.display = "none";
+}
+
+function close_gameSection() {
+  gameSec.style.display = "none";
+  gameZone.style.display = "block";
+}
+
+function open_gameSection() {
+  gameSec.style.display = "block";
+  gameZone.style.display = "none";
+  paper.style.display = "none";
+  scissor.style.display = "none";
+  stone.style.display = "none";
+  resetComputerChoice(); // Reset the computerChoice when opening the game section
+}
+
+// Earlier, same numbers were repeating so I used a do-while loop to get unique numbers
+function generateUniqueRandomNumber() {
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+  } while (randomNumber === previousRandomNumber);
+  previousRandomNumber = randomNumber;
+  return randomNumber;
+}
